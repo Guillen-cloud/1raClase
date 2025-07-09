034da6e935366bd2af40d1d6a16932f32298e07a
Guillen-cloud <guillenbejaranor@gmail.com>
2025-07-08 21:12:45 -0400

    Ignorar carpeta obj

---
+++ apiServicio/apiServicio/Business/Contracts/IRolRepository.cs
@@ -0,0 +1,11 @@
+﻿using apiServicio.Models;
+
+namespace apiServicio.Business.Contracts
+{
+    public interface IRolRepository
+    {
+        Task<Rol> AgregaActualiza(Rol l, string t);
+        Task<List<Rol>> GetList();
+        
+    }
+}
